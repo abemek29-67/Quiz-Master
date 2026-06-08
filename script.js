f4d8e4c53e732623699dcfb74c25d96143377d76
abemek29-67 <abemek29@bergen.org>
2026-06-07 20:09:03 -0400

    display for the questions and the answers and randomizes answer choices and displays a button for each answer with a click event to check each answer

--- script.js
+++ script.js
@@ -24,3 +24,35 @@ async function startQuiz() {
 
     showQuestion();
 }
+
+function showQuestion() {
+    nextBtn.style.display = "none";
+
+    const question = questions[currentQuestion];
+
+    questionElement.innerHTML =
+        `Question ${currentQuestion + 1}: ${question.question}`;
+
+    answersElement.innerHTML = "";
+
+    let answers = [
+        ...question.incorrect_answers,
+        question.correct_answer
+    ];
+
+    answers.sort(() => Math.random() - 0.5);
+
+    answers.forEach(answer => {
+        const button = document.createElement("button");
+
+        button.textContent = answer;
+        button.classList.add("btn");
+        button.style.display = "block";
+        button.style.margin = "10px auto";
+
+        button.addEventListener("click", () => selectAnswer(button, answer));
+
+        answersElement.appendChild(button);
+    });
+}
+
